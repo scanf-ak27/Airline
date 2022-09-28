5fcfb9e69069759cdaeb690b102d65a2fddd72b5
scanf-ak27 <96355521+scanf-ak27@users.noreply.github.com>
2022-09-28 14:19:55 +0530

    Add files via upload

---
+++ conn.java
@@ -0,0 +1,21 @@
+package airline.management.system;
+
+import java.sql.*;  
+
+public class conn{
+    Connection c;
+    Statement s;
+    public conn()
+    {  
+        try{  
+            Class.forName("com.mysql.cj.jdbc.Driver");  
+            c = DriverManager.getConnection("jdbc:mysql://localhost:3306/airline","root","kraayush2704");    
+            s =c.createStatement(); 
+           
+          
+            
+        }catch(Exception e){ 
+            System.out.println(e);
+        }  
+    }  
+}  
